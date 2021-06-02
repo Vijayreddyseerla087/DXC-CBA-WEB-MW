425bce3c49956031121cda3c8355783aeb6f60b1
Vijay Reddy Seerla <84446622+Vijayreddyseerla087@users.noreply.github.com>
2021-06-02 16:56:13 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -13,30 +13,5 @@ echo "Checking Large files under /var/mqm" >> abcd.txt
 
 find /var/mqm -xdev -size  +102400 -ls |sort -r +6 >> abcd.txt
 
-echo "----------------------------------------"
-echo ""
 
-echo "Checking depth and channel status" >> abcd.txt
-
-while read g
-do
-
-echo "dis qs(*) CURDEPTH where(CURDEPTH gt 1000)" |runmqsc $qm |grep -vE 'MQSC|dis|AMQ8450I|Copyright' >> abcd.txt
-
-echo "dis chs(*) where(status ne running) |runmqsc $qm |grep -v AMQ8417 >> abcd.txt
-
-done < qm.txt
-
-echo "----------------------------------------"
-echo ""
-
-echo "Removing 7 days older file from /temp/temps" >> abcd.txt
-
-find /tmp/temps -name '*.FDC' -mtime +7 > qwerty.txt
-
-find /tmp/temps -name '*.FDC' -mtime +7 -exec rm {} \;
-
-echo "Removing 30 days older abcd.txt" >> qwerty.txt
-
-find /var/mqm -name 'abcd.txt' -mtime +30 -exec rm {} \;
 
